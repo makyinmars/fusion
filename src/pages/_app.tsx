a023ca55bb47211a49a03e3b2045c602a26a2ae2
Franklin Jara <franklinjaradev@gmail.com>
2023-08-13 21:14:11 -0400

    :sparkles: feat: setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { api } from "src/utils/api";
+import "src/styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
